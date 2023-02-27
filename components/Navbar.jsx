7ca2e0c179d3bfa5b8d6759d99501b4e90b92ef1
Ntami Egbe <ntamiegbe00@gmail.com>
2023-02-27 12:00:48 +0100

    Commit: Changes to next config file

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,9 +1,24 @@
+import Image from 'next/image'
 import React from 'react'
 
 const Navbar = () => {
-  return (
-    <div>Navbar</div>
-  )
+    return (
+        <div className='flex px-6 py-3 items-center justify-between max-w-6xl mx-auto'>
+
+            <div className="w-4 h-4">
+                <Image src='https://links.papareact.com/ocw' fill/>
+            </div>
+
+            <div className="">
+                2
+            </div>
+
+            <div className="">
+                3
+            </div>
+
+        </div>
+    )
 }
 
 export default Navbar
