76d6e740d474407decf06d7d3e6f6fa6a857f3b2
Ntami Egbe <ntamiegbe00@gmail.com>
2023-02-27 21:26:16 +0100

    Commit: Creating stories component

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -2,10 +2,16 @@ import Stories from './Stories'
 
 const Feed = () => {
   return (
-    <main>
-        <section>
+    <main className='grid grid-cols-1 md:grid-cols-2 md:max-w-3xl xl:grid-cols-3 xl:max-w-6xl mx-auto'>
+
+        <section className='cols-span-2'>
             <Stories />
         </section>
+
+        <section className=''>
+            <h1>Sidebar</h1>
+        </section>
+
     </main>
   )
 }
--- components/Stories.jsx
+++ components/Stories.jsx
@@ -1,8 +1,33 @@
+import { useState, useEffect } from 'react';
+import { faker } from '@faker-js/faker';
 
 const Stories = () => {
+
+    const [peopleData, setPeopleData] = useState([]);
+
+    useEffect(() => {
+        // generate 15 fake people data
+        const data = Array.from({ length: 25 }, () => ({
+            name: faker.name.fullName(),
+            picture: faker.image.avatar(),
+        }));
+        
+        setPeopleData(data);
+    }, []);
+
   return (
-    <div>Stories</div>
-  )
+      <div>
+          <h1>Fake People Data</h1>
+          <ul>
+              {peopleData.map((person) => (
+                  <li key={person.name}>
+                      <img src={person.picture} alt="Profile" />
+                      {person.name}
+                  </li>
+              ))}
+          </ul>
+      </div>
+    )
 }
 
 export default Stories
