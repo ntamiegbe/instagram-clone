0861a40073592a9285e1d44aac75fc3f67bb3be0
Ntami Egbe <ntamiegbe00@gmail.com>
2023-03-01 01:06:20 +0100

    Commit: Styled sign in page

--- pages/auth/signin.js
+++ pages/auth/signin.js
@@ -1,17 +1,26 @@
 import { getProviders, signIn as signToProvider } from "next-auth/react"
+import Navbar from "../../components/Navbar"
+import Image from "next/image"
 
 const signIn = ({ providers }) => {
   return (
     <div className=''>
-      {
-        Object.values(providers).map((provider) => (
-          <div className="" key={provider.name}>
-            <button onClick={() => signToProvider(provider.id)}>
-              Sign in with {provider.name}
-            </button>
-          </div>
-        ))
-      }
+      <Navbar />
+      <div className="flex flex-col items-center justify-center h-screen -mt-40">
+
+        <Image src="https://links.papareact.com/jjm" width={100} height={100} alt="Logo"/>
+        <div className="mt-20">
+          {
+            Object.values(providers).map((provider) => (
+              <div className="" key={provider.name}>
+                <button onClick={() => signToProvider(provider.id)} className="flex shadow-sm px-4 py-2 items-center border rounded-md font-semibold hover:scale-105 transition-all duration-150 ease-out">
+                  <img src="https://rb.gy/nj8u4n" alt="Logo" className="w-10 h-10 mr-2" /> Sign in with {provider.name}
+                </button>
+              </div>
+            ))
+          }
+        </div>
+      </div>
     </div>
   )
 }
