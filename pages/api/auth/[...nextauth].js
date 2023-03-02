03f8f2c4978c883da197c83400249ca669157a81
Ntami Egbe <ntamiegbe00@gmail.com>
2023-03-02 22:00:36 +0100

    Commit: Adding next auth secret key

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,13 +1,8 @@
 import NextAuth from 'next-auth'
-// import GitHub from 'next-auth/providers/github'
 import Google from 'next-auth/providers/google'
 
 export default NextAuth({
     providers: [
-        // GitHub({
-        //     clientId: process.env.GITHUB_CLIENT_ID,
-        //     clientSecret: process.env.GITHUB_CLIENT_SECRET
-        // }),
         Google({
             clientId: process.env.GOOGLE_CLIENT_ID,
             clientSecret: process.env.GOOGLE_CLIENT_SECRET
@@ -27,5 +22,6 @@ export default NextAuth({
             session.user.uid = token.sub
             return session
         }
-    }
+    },
+    secret: process.env.NEXTAUTH_SECRET
 })
